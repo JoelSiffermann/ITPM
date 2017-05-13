4be60d91719e092ffe7eaa73a4f5c022dfa796b9
SaminaAhmed <sa056@hdm-stuttgart.de>
2017-05-13 16:56:54 +0200

    Mapper angelegt

---
+++ src/de/hdm/itprojekt/projektmarktplatz/server/db/AusschreibungMapper.java
@@ -0,0 +1,11 @@
+package de.hdm.itprojekt.projektmarktplatz.server.db;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class AusschreibungMapper {
+	
+}
